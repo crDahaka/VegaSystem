2d4e82b5f8cdd04ed85d30496c578c079a7a8b90
crDahaka <bkristian7@gmail.com>
2017-11-12 22:50:20 +0200

    Implement the cascade behaviour in make drop-down list

--- VegaSystem/ClientApp/app/app.module.shared.ts
+++ VegaSystem/ClientApp/app/app.module.shared.ts
@@ -1,9 +1,10 @@
-import { MakeService } from './services/make.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router'
+
+import { MakeService } from './services/make.service';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
