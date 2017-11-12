db330d16da7db5871cf3a83441b446383bb0ce9d
crDahaka <bkristian7@gmail.com>
2017-11-12 23:59:38 +0200

    Asociate vehicles with features

--- VegaSystem/Entities/Vehicle.cs
+++ VegaSystem/Entities/Vehicle.cs
@@ -1,8 +1,12 @@
 namespace VegaSystem.Entities
 {
     using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
+    [Table("Vehicles")]
     public class Vehicle
     {
         public int Id { get; set; }
@@ -25,5 +29,12 @@ namespace VegaSystem.Entities
         public string ContactPhone { get; set; }
 
         public DateTime LastUpdate { get; set; }
+
+        public ICollection<VehicleFeature> Features { get; set; }
+
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
     }
 }
--- VegaSystem/Migrations/20171112215842_AddVehicle.Designer.cs
+++ VegaSystem/Migrations/20171112215842_AddVehicle.Designer.cs
@@ -11,9 +11,10 @@ using VegaSystem.Persistence;
 namespace VegaSystem.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171112215842_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
