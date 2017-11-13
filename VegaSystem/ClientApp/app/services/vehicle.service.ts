89ba7cf912af7a9eca8616ca0af8148c1a9f2ca1
crDahaka <bkristian7@gmail.com>
2017-11-13 22:30:28 +0200

    Bind the vehicle model and implement client side validation

--- VegaSystem/ClientApp/app/services/vehicle.service.ts
+++ VegaSystem/ClientApp/app/services/vehicle.service.ts
@@ -8,13 +8,15 @@ export class VehicleService {
   constructor(private http: Http) { }
 
   getMakes(){
-    return this.http.get('/api/makes')
-      .map(res => res.json());
+    return this.http.get('/api/makes').map(res => res.json());
   }
 
   getFeatures(){
-    return this.http.get('/api/features')
-      .map(res => res.json());
+    return this.http.get('/api/features').map(res => res.json());
+  }
+
+  create(vehicle){
+    return this.http.post('/api/vehicles', vehicle).map(res => res.json());
   }
   
 }
