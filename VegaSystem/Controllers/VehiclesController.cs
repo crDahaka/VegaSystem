0534dabfc29856d01602fe66747792c18f3d7941
crDahaka <bkristian7@gmail.com>
2017-11-13 11:53:23 +0200

    Test CreateVehicle action

--- VegaSystem/Controllers/VehiclesController.cs
+++ VegaSystem/Controllers/VehiclesController.cs
@@ -1,25 +1,37 @@
 namespace VegaSystem.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
     using AutoMapper;
     using Microsoft.AspNetCore.Mvc;
     using VegaSystem.Entities;
+    using VegaSystem.Persistence;
     using VegaSystem.Resources;
 
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
         private readonly IMapper mapper;
+        private readonly VegaDbContext context;
 
-        public VehiclesController(IMapper mapper)
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
+            this.context = context;
             this.mapper = mapper;
 
         }
 
-        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-            return Ok(vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
         }
     }
 }
